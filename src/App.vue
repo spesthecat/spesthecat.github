fd370b5c7b9978166e669c37e192062d5ea11b6d
spesthecat <kenhtsun@gmail.com>
2020-07-22 23:05:57 -0400

    so much progress lmao

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+
+		<router-view/>
+	</div>
 </template>
 
 <style lang="scss">
+
+* {
+	margin: 0;	
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	background-color: rgba(0, 0, 0, 0.911);
+	position: absolute;
+	width: 100%;
+	height: 100%;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
---
+++ src/components/menu.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id='menu'>
+				
+	</div>
+</template>
+
+<script>
+
+export default {
+	name: 'menu'
+}
+
+</script>
+
+<style scoped>
+
+</style>
