2a33b327ae1aa24b0be609fc70f34ea11b85a6c4
Kenneth Sun <kenhtsun@gmail.com>
2020-12-19 08:37:11 -0500

    back to target: static

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,6 @@
 export default {
-  // Target (https://go.nuxtjs.dev/config-target)
-  // target: 'static',
+  // Target (https://go.nuxtjs.dev/config-target
+  target: 'static',
   
   buildDir: 'dist',
   
