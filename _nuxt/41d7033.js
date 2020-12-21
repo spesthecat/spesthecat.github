27c4aa2d5057345b95fcb67c9c93427ad0fc4e38
Kenneth Sun <kenhtsun@gmail.com>
2020-12-21 09:10:57 -0500

    deploy

---
+++ _nuxt/41d7033.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{330:function(o,t,e){"use strict";e.r(t);e(42),e(63);var n={mounted:function(){this.$route.params.category||this.$route.params.id||this.home()},methods:{home:function(){this.$router.replace("/projects/home")}}},r=e(41),component=Object(r.a)(n,void 0,void 0,!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/86f251f.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{331:function(o,n,t){"use strict";t.r(n);var e={mounted:function(){this.$router.push("/projects/home")}},c=t(41),component=Object(c.a)(e,void 0,void 0,!1,null,null,null);n.default=component.exports}}]);
